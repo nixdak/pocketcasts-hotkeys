9afd80ee0ce655ae9cc93573727d8e5d184ec539
Craig Gavagan <craig@craiggavagan.com>
2018-03-09 14:32:53 +0000

    Adding ability to slow/speed up playback

--- src/background.js
+++ src/background.js
@@ -4,6 +4,8 @@ const pocketCastsUrl = 'https://playbeta.pocketcasts.com/*';
 const togglePlaybackCommand = 'toggle-playback';
 const skipEpisodeBackwardCommand = 'skip-episode-backward';
 const skipEpisodeForwardCommand = 'skip-episode-forward';
+const decreasePlaybackSpeedCommand = 'decrease-playback-speed';
+const increasePlaybackSpeedCommand = 'increase-playback-speed';
 
 async function openPocketCasts() {
     await browser.tabs.create({
@@ -20,6 +22,10 @@ function scriptFor(command) {
             return scriptThatClicksOn('skip_back_button');
         case skipEpisodeForwardCommand:
             return scriptThatClicksOn('skip_forward_button');
+        case decreasePlaybackSpeedCommand:
+            return scriptThatClicksOn('minus-button');
+        case increasePlaybackSpeedCommand:
+            return scriptThatClicksOn('plus-button');
     }
 }
 
@@ -71,4 +77,23 @@ browser.contextMenus.create({
     title: 'Skip Forward',
     contexts: ['browser_action'],
     onclick: () => executePocketCastsCommand(skipEpisodeForwardCommand),
+});
+
+browser.contextMenus.create({
+    id: 'playback-separator',
+    type: 'separator'
+});
+
+browser.contextMenus.create({
+    id: 'decrease-playback-speed',
+    title: 'Decrease playback speed',
+    contexts: ['browser_action'],
+    onclick: () => executePocketCastsCommand(decreasePlaybackSpeedCommand)
+});
+
+browser.contextMenus.create({
+    id: 'increase-playback-speed',
+    title: 'Increase playback speed',
+    contexts: ['browser_action'],
+    onclick: () => executePocketCastsCommand(increasePlaybackSpeedCommand)
 });
